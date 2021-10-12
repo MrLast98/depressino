284e4325eb64759d5428839142dc08d4115f3054
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2021-10-12 10:18:18 +0200

    base comune da cui iniziare

--- depressino.playground/Contents.swift
+++ depressino.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+import SwiftUI
+import PlaygroundSupport
 
 var greeting = "Hello, playground"
+
+struct MyView: View {
+    @State private var animationAmount: Bool = false
+
+    var body: some View {
+        Button("Suca") {
+            self.animationAmount = !self.animationAmount
+            //self.animationAmount = true
+        }
+        .padding(50)
+        .background(Color.blue)
+        .foregroundColor(Color.black)
+        Image(uiImage: UIImage(named: "2021-09-30 23.46.18.jpg")!)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 400)
+            .scaleEffect(animationAmount ? 1.25 : 0.25)
+            .animation(.spring())
+            
+    }
+}
+PlaygroundSupport.PlaygroundPage.current.setLiveView(MyView());
