98f85ed432982cd08a0c389b840e86f47d618608
Emanuele Giunta <egindustries11@gmail.com>
2021-10-13 11:40:04 +0200

    Adding the new stuff

--- depressino.playground/Contents.swift
+++ depressino.playground/Contents.swift
@@ -1,26 +1,90 @@
-import SwiftUI
+import UIKit
 import PlaygroundSupport
+import SwiftUI
+import CoreGraphics
 
-var greeting = "Hello, playground"
+public struct WhatsNewView<Content: View>: View {
+    private let content: Content
 
-struct MyView: View {
-    @State private var animationAmount: Bool = false
+    public init(@ViewBuilder contentProvider: () -> Content){
+        content = contentProvider()
+    }
 
-    var body: some View {
-        Button("Suca") {
-            self.animationAmount = !self.animationAmount
-            //self.animationAmount = true
+    public var body: some View {
+        TabView {
+            content
         }
-        .padding(50)
-        .background(Color.blue)
-        .foregroundColor(Color.black)
-        Image(uiImage: UIImage(named: "2021-09-30 23.46.18.jpg")!)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 400)
-            .scaleEffect(animationAmount ? 1.25 : 0.25)
-            .animation(.spring())
-            
+        .background(Color.white)
+        .frame(width: 500, height: 500, alignment: .center)
+        .ignoresSafeArea()
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
-PlaygroundSupport.PlaygroundPage.current.setLiveView(MyView());
+
+struct myView: View {
+    @State private var tabIndex = 0
+    var body: some View {
+        TabView(selection: $tabIndex) {
+                    firstView(tabIndex: $tabIndex)
+                        .tabItem {
+                            Text("Opening")
+                        }
+                        .tag(0)
+                    secondView(tabIndex: $tabIndex)
+                        .tabItem {
+                            Text("Chap. 1")
+                        }
+                        .tag(1)
+                }
+        }
+    }
+
+struct firstView: View {
+    @Binding var tabIndex: Int
+    @State var animationAmount : Bool = false
+    
+    struct MyButtonStyle: ButtonStyle {
+        public func makeBody(configuration: MyButtonStyle.Configuration) -> some View {
+            configuration.label
+                .opacity(configuration.isPressed ? 1 : 1)
+                .scaleEffect(configuration.isPressed ? 0.8 : 1)
+        }
+    }
+
+    var body: some View {
+        
+        Button(action: {
+            withAnimation(.linear(duration: 1.0)) {
+                self.animationAmount = !self.animationAmount
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.tabIndex += 1
+                }
+            }
+            
+        }){
+            Image(uiImage: UIImage(named: "ciaone")!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400)
+                .opacity(animationAmount ? 0 : 1)
+                
+        }.buttonStyle(MyButtonStyle())
+            .onAppear(perform: {
+                self.animationAmount = false
+                self.tabIndex = 0
+            })
+            .tag(0)
+    }
+}
+struct secondView: View {
+    @Binding var tabIndex: Int
+        var body: some View {
+            Button(action: {
+                self.tabIndex += 1;
+            }) {
+                Text("Change to tab 2")
+            }
+        }
+}
+PlaygroundSupport.PlaygroundPage.current.setLiveView(myView());
